601f7b453571c62b5f1754e6f6b4d8b59fac67c8
Ruchika Chavhan <32021556+bayesianGirl@users.noreply.github.com>
2019-01-03 05:49:52 +0000

    training

--- expt.py
+++ expt.py
@@ -45,10 +45,10 @@ class ConvNet(nn.Module):
 
 class Net(nn.Module):
 	def __init__(self):
-	super(Net, self).__init__()
-	self.fc1 = nn.Linear(256, 64)
-	self.fc2 = nn.Linear(64, 32)
-	self.fc3 = nn.Linear(32, 100)
+		super(Net, self).__init__()
+		self.fc1 = nn.Linear(256, 64)
+		self.fc2 = nn.Linear(64, 32)
+		self.fc3 = nn.Linear(32, 100)
 
 	def forward(self, x):
 		x = F.relu(self.fc1(x))
@@ -56,41 +56,39 @@ class Net(nn.Module):
 		x = self.fc3(x)
 		return x
 
-optimizer = optim.Adam(params_total, lr=0.0001)
-	def train(Z, Phi, target):
-	for iterations in range(0, 10000):
-	avg_loss=0
-	for i in range(0, Z_.shape[1], 100):
-		z= return_tensor(Z, i)
-		phi= return_tensor(Phi, i)
-		Conv_Net_output_Z= ConvNet.forward(z)
-		Conv_Net_output_Phi= ConvNet.forward(phi)
-		FirstLayer= np.append(Conv_Net_output_Z, Conv_Net_output_Phi, axis =1)
-		output= forward_net.forward(FirstLayer)
-		criteria= nn.MSELoss()
-		label= return_tensor(target, i)
-		loss= criteria(output, label)
-		avg_loss= avg_loss+loss
-		optimizer.zero_grad() 
-		loss.backward()
-		optimizer.step() 
-	print(iterations+1, avg_loss.item())
-	if(iterations%10 ==0):
-		name= "model_"+ str(iterations)+ ".pt"
-		torch.save(net, "/home/sine/ruchika/"+ name)
-
 conv_net=ConvNet().to(device)
 forward_net= Net().to(device)
 params1 = list(conv_net.parameters())
 conv_net.zero_grad()
 params2 = list(forward_net.parameters())
 forward_net.zero_grad()
-params_total= []
-params_total.append(params1)
-params_total.append(params2)
+params_total= np.append(params1, params2)
+optimizer = optim.Adam(params_total, lr=0.0001)
+
+def train(Z, Phi, target):
+	for iterations in range(0, 10000):
+		avg_loss=0
+		for i in range(0, Z_.shape[1], 100):
+			z= return_tensor(Z, i)
+			phi= return_tensor(Phi, i)
+			Conv_Net_output_Z= ConvNet.forward(z)
+			Conv_Net_output_Phi= ConvNet.forward(phi)
+			FirstLayer= np.append(Conv_Net_output_Z, Conv_Net_output_Phi, axis =1)
+			output= forward_net.forward(FirstLayer)
+			criteria= nn.MSELoss()
+			label= return_tensor(target, i)
+			loss= criteria(output, label)
+			avg_loss= avg_loss+loss
+			optimizer.zero_grad() 
+			loss.backward()
+			optimizer.step() 
+		print(iterations+1, avg_loss.item())
+		if(iterations%10 ==0):
+			name= "model_"+ str(iterations)+ ".pt"
+			torch.save(net, "/home/sine/ruchika/"+ name)
 
 #Reading data from csv file and dividing into inner and outer channels 
-data = np.genfromtxt("antihydrogen_1-10000.csv")
+data = np.genfromtxt("antihydrogen_test.csv")
 #Ground truth 
 target= data[:,:1]
 InnerZ= data[:,1:448]
